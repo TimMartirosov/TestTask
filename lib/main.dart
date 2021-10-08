ef238fc3dab84882494bb57f437c0c6c78221895
TimMartirosov <Tymur.martirosov@nure.ua>
2021-10-08 15:13:26 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:test_aplication/generated/l10n.dart';
 import 'package:test_aplication/screens/home.dart';
+import 'package:test_aplication/theme.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: TestApp(),
-  ));
+  runApp(AdaptiveTheme(
+      initial: AdaptiveThemeMode.light,
+      light: kLightMode,
+      dark: kDarkMode,
+      builder: (light, dark) => MaterialApp(
+            theme: light,
+            darkTheme: dark,
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            home: const TestApp(),
+          )));
 }
